6e981fced5dc0bea80a550206826905162de21fa
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-04-21 09:43:33 -0500

    fixed stuff for npm

--- src/main.js
+++ src/main.js
@@ -3,9 +3,22 @@ import fs from 'fs';
 import ncp from 'ncp';
 import path from 'path';
 import { promisify } from 'util';
+import execa from 'execa';
+import Listr from 'listr';
+import { projectInstall } from 'pkg-install';
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
+
+async function initGit(options) {
+    const result = await execa('git', ['init'], {
+      cwd: options.targetDirectory,
+    });
+    if (result.failed) {
+      return Promise.reject(new Error('Failed to initialize git'));
+    }
+    return;
+   }
 
 async function copyTemplateFiles(options) {
  return copy(options.templateDirectory, options.targetDirectory, {
@@ -33,6 +46,30 @@ export async function createProject(options) {
    console.error('%s Invalid template name', chalk.red.bold('ERROR'));
    process.exit(1);
  }
+ const tasks = new Listr([
+    {
+      title: 'Copy project files',
+      task: () => copyTemplateFiles(options),
+    },
+    {
+      title: 'Initialize git',
+      task: () => initGit(options),
+      enabled: () => options.git,
+    },
+    {
+      title: 'Install dependencies',
+      task: () =>
+        projectInstall({
+          cwd: options.targetDirectory,
+        }),
+      skip: () =>
+        !options.runInstall
+          ? 'Pass --install to automatically install dependencies'
+          : undefined,
+    },
+  ]);
+ 
+  await tasks.run();
 
  console.log('Copy project files');
  await copyTemplateFiles(options);
