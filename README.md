b6e48e003666a7bfdb82df21b86dc4a792e52af7
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-04-21 09:25:44 -0500

    completed basic template setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # the_brybry_cli
 this is a cli that I will set up that will allow me to bootstrap different web projects and configurations such as react, vue, react-redux, vuex, express/pg/knex, react-mobx, react-context, react-three, electronjs, and potentially more.
+
---
+++ templates/react-basic/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>Hope you enjoy the brybry cli</h2>
+    </div>
+  );
+}
+
+export default App;
